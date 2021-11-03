7338b9d4bf87766de29725e9d67abe83a0523253
namphung99 <namphungptit2204@gmail.com>
2021-11-03 23:33:01 +0700

    create code base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateArticleComponent } from './feature-modules/create-article/create-article.component';
+import { HomeComponent } from './feature-modules/home/home.component';
+import { LoginComponent } from './feature-modules/login/login.component';
+import { ProfileComponent } from './feature-modules/profile/profile.component';
+import { RegisterComponent } from './feature-modules/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"login",
+    component: LoginComponent,
+  },
+  {
+    path:"register",
+    component: RegisterComponent,
+  },
+  {
+    path:"articles",
+    component: CreateArticleComponent,
+  },
+  {
+    path:"/:username",
+    component:ProfileComponent,
+  },
+  {
+    path:"",
+    component: HomeComponent,
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { LoginComponent } from './feature-modules/login/login.component';
+import { RegisterComponent } from './feature-modules/register/register.component';
+import { ArticleComponent } from './share-modules/article/article.component';
+import { CreateArticleComponent } from './feature-modules/create-article/create-article.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './feature-modules/profile/profile.component';
+import { FavoritesComponent } from './feature-modules/favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ArticleComponent,
+    CreateArticleComponent,
+    ProfileComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
