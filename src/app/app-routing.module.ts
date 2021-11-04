5688fc1ff32bcdbd879aceb0fc2460d0ec1b2067
namphung99 <namphungptit2204@gmail.com>
2021-11-04 18:40:48 +0700

    add home page and styles folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateArticleComponent } from './feature-modules/create-article/create-article.component';
 import { HomeComponent } from './feature-modules/home/home.component';
-import { LoginComponent } from './feature-modules/login/login.component';
+import { LoginComponent } from './feature-modules/Auth/login/login.component';
 import { ProfileComponent } from './feature-modules/profile/profile.component';
-import { RegisterComponent } from './feature-modules/register/register.component';
+import { RegisterComponent } from './feature-modules/Auth/register/register.component';
 
 const routes: Routes = [
 
+  {
+    path: '',
+    redirectTo: "home",
+    pathMatch: 'full',
+  },
   {
     path:"login",
     component: LoginComponent,
@@ -21,12 +26,13 @@ const routes: Routes = [
     component: CreateArticleComponent,
   },
   {
-    path:":username",
+    path:"profile/:username",
     component:ProfileComponent,
   },
   {
     path:"home",
-    component: HomeComponent,
+    loadChildren: () => import("./feature-modules/home/home.module")
+    .then(response => response.HomeModule)
   }
 ];
 
