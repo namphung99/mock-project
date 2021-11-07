3d06660f5aa1ac2d0987e45069ae029f2a28fcab
yuta <yuta.nguyen25@gmail.com>
2021-11-07 13:25:24 +0700

    Create UI article detail when User doesn't login

--- src/app/feature-modules/article-detail/article-detail.component.ts
+++ src/app/feature-modules/article-detail/article-detail.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleDetailComponent implements OnInit {
 
+  article = {
+    avatar: 'https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-11.jpg',
+    username: 'trieu',
+    publishedDate: '10/10/2012',
+    like: 18,
+    title: "This is title of page",
+    description: "This is description",
+    body: "This is body",
+    tags: ['tag1', 'tag2']
+  };
+
   constructor() { }
 
   ngOnInit() {
--- src/app/feature-modules/article-detail/article-detail.module.ts
+++ src/app/feature-modules/article-detail/article-detail.module.ts
@@ -1,9 +1,16 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleDetailComponent } from './article-detail.component';
 
 @NgModule({
   imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ArticleDetailComponent
+      }
+    ]),
     CommonModule
   ],
   declarations: [ArticleDetailComponent]
