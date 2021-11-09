3255c4b082a04a3352add40c17fe5d4f7c77fbe8
namphung99 <namphungptit2204@gmail.com>
2021-11-09 11:42:55 +0700

    handle isLoggedIn in home page

--- src/app/feature-modules/home/home.component.ts
+++ src/app/feature-modules/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ArticleService } from 'src/app/services/article.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { ArticleGet, ArticlePost } from 'src/app/shares/interfaces/article.interface';
 import { ModalArticleComponent } from '../../share-modules/modal-article/modal-article.component';
 
@@ -10,17 +11,17 @@ import { ModalArticleComponent } from '../../share-modules/modal-article/modal-a
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public token!: any;
   public articles: ArticleGet[] = []
-  isLoggedIn: boolean = false;
+  public isLoggedIn: boolean = false;
   public imgUrl: string = "https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-11.jpg"
   constructor(
     private modalService: NgbModal,
-    private articleService: ArticleService
+    private articleService: ArticleService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
-    this.token = localStorage.getItem('token')
+    this.isLoggedIn = this.authService.getIsLoggedIn();
     this.articleService.getArticles();
     this.articleService.emitArticle.subscribe((res:ArticleGet[]) => {
       this.articles = res
